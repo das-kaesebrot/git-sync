166f2a76d336201f6b8acea0e137bb361de52ce8
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2022-11-05 14:19:29 +0100

    implementation of GitRemote class

--- src/gitremote.py
+++ src/gitremote.py
@@ -1,5 +1,10 @@
 class GitRemote:    
     is_source: bool
+    remote_url: str
+    name: str
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, name, remote_url, is_source: bool = False) -> None:
+        self.name = name
+
+        self.remote_url = remote_url
+        self.is_source = is_source
