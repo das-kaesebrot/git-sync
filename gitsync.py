af833fe58b4711e0460327bcc8ee7ad0828b837a
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2022-11-05 15:03:11 +0100

    add scheduled and one-time execution

--- gitsync.py
+++ gitsync.py
@@ -2,14 +2,29 @@
 
 import sys
 import logging
+import time
 
 from src.config import Config
+from src.gitsynchelper import GitSyncHelper
 
 def main():
     logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s', level=logging.DEBUG)
 
     config = Config()
     config.print()
+
+    helper = GitSyncHelper(config.FILECONF.get("repos"), config.ARGS.get("cache_root_dir"), config.keyfile_root)
+    cron = config.CRON_INTERVAL
+
+    if config.run_cron:
+        while True:
+            sleeptime = cron.next(default_utc=True)
+            logging.info(f"Sleeping for {sleeptime}s until next cron job run")
+            time.sleep(sleeptime)
+            helper.sync_all()
+
+    else:
+        helper.sync_all()
 
 if __name__ == "__main__":
     try:
