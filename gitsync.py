b1522c6f7b9a82ea250a4b13d1cad175ca32edf2
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2022-11-05 11:53:02 +0100

    [FEATURE] entrypoint and main method

--- gitsync.py
+++ gitsync.py
@@ -1,2 +1,24 @@
 #!/usr/bin/env python3
 
+import sys
+import logging
+
+from src.config import Config
+
+def main():
+    logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s', level=logging.DEBUG)
+
+    config = Config()
+    config.print()
+
+if __name__ == "__main__":
+    try:
+        main()
+        
+    except (KeyboardInterrupt, InterruptedError) as e:
+        print(f"{e=}")
+        sys.exit(0)
+        
+    except Exception as e:
+        print(f"Encountered exception: {e=}")
+        sys.exit(1)
