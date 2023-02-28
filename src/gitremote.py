0fcf1963450b624d37196eef9b1bacd387d2b44d
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-02-28 16:55:05 +0100

    add excluded refs prop

--- src/gitremote.py
+++ src/gitremote.py
@@ -5,12 +5,14 @@ class GitRemote:
     remote_url: str
     name: str
     keyfile: str = None
+    excluded_refs: list[str]
 
-    def __init__(self, name, remote_url, is_source: bool = False, keyfile: str = None) -> None:
+    def __init__(self, name, remote_url, is_source: bool = False, keyfile: str = None, excluded_refs: list[str] = None) -> None:
         self.name = name
 
         self.remote_url = remote_url
         self.is_source = is_source
+        self.excluded_refs = excluded_refs
         
         if keyfile:
             if not os.path.isfile(keyfile):
